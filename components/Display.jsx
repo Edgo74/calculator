6d5537c6dce133770ca5cd51377468ba333a3455
adam <adambayar1357@gmail.com>
2024-10-21 10:38:31 +0200

    first commit

---
+++ components/Display.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import getNum from "../utilities/getNum"
+
+export default function Display({ currentCalc }) {
+    
+    const previousCalc = React.useRef()
+    
+    const { previousNum, currentNum, result } = currentCalc
+    
+    let numToDisplay = result.join("") || currentNum.join("") || previousNum.join("") || "0"
+    
+    if (Array.isArray(numToDisplay)) {
+        console.log('fired')
+        numToDisplay = numToDisplay[0]
+    }
+    
+    numToDisplay = numToDisplay.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") 
+
+    const numLength = numToDisplay.length
+    
+    const style = numLength > 7 ? {fontSize: calculateFontSize(numLength)} : null
+    
+    
+    function calculateFontSize(numLength) {
+        
+        const excess = numLength - 6
+        const calculatedSize = 50 - excess * 3.5
+        const fontSize = calculatedSize > 18 ? calculatedSize : 18
+        return `${fontSize}px`
+    }
+
+
+  return <div style={style} className="display">{numToDisplay}</div>
+}
